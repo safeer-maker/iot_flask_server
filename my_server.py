39030593a2298b56663d8099bc8d2a3573686945
Safeer <bsee15055@itu.edu.pk>
2020-07-04 14:46:46 +0000

    String Read
    Read string from url

--- my_server.py
+++ my_server.py
@@ -1,11 +1,19 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
 
+
+@app.route("/query", methods=['GET'])
+def query():
+    sam = request.args.get('name')
+    return jsonify(
+        sam
+    )
 app.run()
